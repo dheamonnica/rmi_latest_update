9ca1f5d13f4c6ed8f842f2fecc53054ee98f2a50
dheamonnica <dheamonnica.acc@gmail.com>
2024-07-20 00:02:45 +0700

    fix: crm in prod

--- resources/views/admin/crm/partials/status.blade.php
+++ resources/views/admin/crm/partials/status.blade.php
@@ -1,5 +1,7 @@
-@if ((($crm->total_plan_actual / $crm->total_plan) * 100) >= 100)
-    <td><span class="label label-primary">ACHIEVE</span></td>
-@else
-    <td><span class="label label-danger">FAIL</span></td>
+@if ($crm->total_plan_actual || $crm->total_plan)
+    @if (($crm->total_plan_actual / $crm->total_plan) * 100 >= 100)
+        <td><span class="label label-primary">ACHIEVE</span></td>
+    @else
+        <td><span class="label label-danger">FAIL</span></td>
+    @endif
 @endif
