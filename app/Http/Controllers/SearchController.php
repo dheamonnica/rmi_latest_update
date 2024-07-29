743457218e0d425aa9c9a177005178f8a76e98e0
Aldy Gumilar <aldy.satria07@gmail.com>
2024-07-29 21:05:07 +0700

    fix: :ambulance: searchable all cust for SU

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -78,17 +78,23 @@ class SearchController extends Controller
 
         // $customers = Customer::search($term)->where('active', 1)->take(5)->get();
         //find user warehouses service & Unit And Aksesoris
-        $warehouse = User::
-            where('warehouse_name', 'LIKE', '%SERVICE%')
-            ->orWhere('warehouse_name', 'LIKE', '%service%')
-            ->orWhere('warehouse_name', 'LIKE', '%Unit%')
-            ->orWhere('warehouse_name', 'LIKE', '%Unit And Aksesoris%')
-            ->orWhere('warehouse_name', 'LIKE', '%Unit And Accessories%')
-            ->get()->pluck('id')->toArray();
 
-        array_push($warehouse, Auth::user()->id);
+        if(Auth::user()->role->level === 1){
+            $customers = Customer::where('name', 'LIKE', '%'.$term.'%')->where('active', 1)->take(5)->get();
+        } else {
+            $warehouse = User::
+                where('warehouse_name', 'LIKE', '%SERVICE%')
+                ->orWhere('warehouse_name', 'LIKE', '%service%')
+                ->orWhere('warehouse_name', 'LIKE', '%Unit%')
+                ->orWhere('warehouse_name', 'LIKE', '%Unit And Aksesoris%')
+                ->orWhere('warehouse_name', 'LIKE', '%Unit And Accessories%')
+                ->get()->pluck('id')->toArray();
 
-        $customers = Customer::where('name', 'LIKE', '%'.$term.'%')->whereIn('merchant_id', $warehouse)->where('active', 1)->take(5)->get();
+            array_push($warehouse, Auth::user()->id);
+
+            $customers = Customer::where('name', 'LIKE', '%'.$term.'%')->whereIn('merchant_id', $warehouse)->where('active', 1)->take(5)->get();
+
+        }
 
         foreach ($customers as $customer) {
             $results[] = ['text' => get_formated_cutomer_str($customer), 'id' => $customer->id];
