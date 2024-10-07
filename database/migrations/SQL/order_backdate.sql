d18669d5256681fb3bbd915ddcbd83c16f39a1a9
Aldy Gumilar <aldy.satria07@gmail.com>
2024-10-07 09:34:36 +0700

    update sql order backdate

--- database/migrations/SQL/order_backdate.sql
+++ database/migrations/SQL/order_backdate.sql
@@ -5,7 +5,7 @@ ADD COLUMN `is_backdate` INT DEFAULT 0;
 -- For the 'order_items' table
 ALTER TABLE `order_items`
 ADD COLUMN `is_backdate` INT DEFAULT 0,
-ADD COLUMN `product_id` INT UNSIGNED,
+ADD COLUMN `product_id` BIGINT UNSIGNED,
 ADD INDEX (`product_id`);
 
 ALTER TABLE `order_items`
