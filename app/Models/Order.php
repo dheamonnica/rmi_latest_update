c84cf05504d6ccd244e16aa42116de81d99d081e
Aldy Gumilar <aldy.satria07@gmail.com>
2024-12-29 19:33:25 +0700

    order images logo fix

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -802,8 +802,8 @@ class Order extends BaseModel
         //http://backend_rmi.test/image/images/66af7419ed6f3.webp
         //echo '<img src="'.get_logo_url('system', 'logo').'" width="30">';
         //dd(get_logo_url('system', 'logo'));
-        $invoice->setLogo(get_logo_url('system', 'logo'), 25, 25);
-        $invoice->setLogo('https://rmi-testing.ideaprojects.my.id/image/images/logo.png', 25, 25);
+        $invoice->setLogo(get_logo_url('system', 'logo'), 75, 75);
+        $invoice->setLogo('https://rmi-testing.ideaprojects.my.id/image/images/logo.png', 75, 75);
 
         $invoice->setReference($this->order_number);   // Reference
         $invoice->setDate($this->created_at->format('M d, Y'));   //Billing Date
